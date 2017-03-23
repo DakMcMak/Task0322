7fc341d317880957e91f7fb6a1306959dd6f0220
DakMcMak <esbolov.galymzhan@mail.ru>
2017-03-23 23:59:11 +0600

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name1 = scanner.nextLine();
+        String name2 = scanner.nextLine();
+        String name3 = scanner.nextLine();
+        System.out.println(name1 + " + " + name2 + " + " + name3 + " = " + "Чистая любовь, да-да! ");
+    }
+}
